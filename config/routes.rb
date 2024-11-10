5668b597cffacbd1cc385acf86e99a98b51c70c9
BenMukebo <l.benkasmukebo7@gmail.com>
2024-11-11 01:20:52 +0200

    Config device auth routes path, Update localhost port to 8000,  and make the app routes accessible only for authenticate_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,23 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  # devise_for :users
+  devise_for :users, path: 'auth',
+                     path_names: {
+                       sign_in: 'login',
+                       sign_out: 'logout',
+                       password: 'secret',
+                       confirmation: 'verification',
+                       unlock: 'unblock',
+                       registration: 'register',
+                       sign_up: 'cmon_let_me_in'
+                     }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'homes#index'
+  get 'homes/index'
 end
