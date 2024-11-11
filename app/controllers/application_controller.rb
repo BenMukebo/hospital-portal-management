d1a2b1b56fda9d3097938cc3e6b22201f8a1c202
BenMukebo <l.benkasmukebo7@gmail.com>
2024-11-11 21:19:54 +0200

    Add  terms_of_service and username column to the user table and config the permit parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username terms_of_service])
+  end
+
+  # def after_sign_out_path_for scope
+  #   root_path
+  # end
 end
