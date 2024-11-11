2d409867b061597d8b71addc3bcfad61658509b7
BenMukebo <l.benkasmukebo7@gmail.com>
2024-11-11 10:55:37 +0200

    Add Role enum column to users table and set the available enum from the user modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :trackable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { patient: 0, receptionist: 1, doctor: 2, admin: 3 }, _prefix: false, _default: :patient, _suffix: true
 end
