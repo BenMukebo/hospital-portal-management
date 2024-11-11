bc4f0f20c723e3e0ed4dd644c008bc18d2abfd94
BenMukebo <l.benkasmukebo7@gmail.com>
2024-11-11 21:22:37 +0200

    Add user columns validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,14 @@ class User < ApplicationRecord
 
   has_one :patient
 
+  validates :email, length: { maximum: 128 },
+                    uniqueness: { case_sensitive: false }, allow_blank: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create, allow_blank: false
+  validates :password_confirmation, presence: true, allow_blank: true
+
+  validates :terms_of_service, acceptance: true
+  validates_inclusion_of :remember_me, in: [true, false], allow_blank: true
+
   enum role: { patient: 0, receptionist: 1, doctor: 2, admin: 3 }.freeze, _prefix: false, _default: :patient
 end
